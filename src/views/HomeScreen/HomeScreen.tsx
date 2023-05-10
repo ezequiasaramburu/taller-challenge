564769c6b7350c6f8e12ef6728d5b8d710a102b7
Ezequias Aramburu <ezearamburu@hotmail.com>
2023-05-10 15:49:22 -0300

    remove test button

--- src/views/HomeScreen/HomeScreen.tsx
+++ src/views/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 import {styles} from './HomeScreen.styles';
 
-export const HomeScreen = ({navigation}) => {
+export const HomeScreen = () => {
   return (
     <View style={styles.center}>
       <Text>HomeScreen Content</Text>
-      <Button
-        title="Go to Screen One"
-        onPress={() => navigation.navigate('ScreenOne')}
-      />
     </View>
   );
 };
--- src/views/ScreenOne/ScreenOne.tsx
+++ src/views/ScreenOne/ScreenOne.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 import {styles} from './ScreenOne.styles';
 
-export const ScreenOne = ({navigation}) => {
+export const ScreenOne = () => {
   return (
     <View style={styles.center}>
       <Text>ScreenOne Content</Text>
-      <Button
-        title="Go to Screen Two"
-        onPress={() => navigation.navigate('ScreenTwo')}
-      />
     </View>
   );
 };
